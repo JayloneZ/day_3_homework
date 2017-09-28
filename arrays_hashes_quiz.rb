d80b51f97d80b815bcda62c6b03777c92b9c22fe
JayloneZ <andrezyczkowski@gmail.com>
2017-09-28 09:32:27 +0100

    removed unnecessary 'return'

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -7,15 +7,15 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 # ```
 
 1. Work out how many stops there are in the array
-  lines.count
+  lines.count()
 2. Return 'Edinburgh Park' from the array
   lines[1]
 3. How many ways can we return 'Princes Street' from the array?
-  return lines[4], return lines[-1], lines.pop()
+  return lines[4], return lines[-1], lines.pop(), lines.fetch(4), lines.last(), lines[lines.length()-1]
 4. Work out the index position of 'Haymarket'
   return lines.index("Haymarket")
 5. Add 'Airport' to the start of the array
-  lines.unshift("Airport")
+  lines.unshift("Airport"), lines.insert(0, "Airport")
 6. Add 'York Place' to the end of the array
   lines.push("York Place")
 7. Remove 'Edinburgh Park' from the array using its name
@@ -23,7 +23,7 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 8. Delete 'Edinburgh Park' from the array by index
   lines.delete_at(1)
 9. Reverse the positions of the stops in the array
-  lines.reverse
+  lines.reverse()
 
 
 ### B. Given the following data structure:
@@ -33,11 +33,11 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 # ```
 
 1. How would you return the string `"One"`?
-  return my_hash[1]
+  my_hash[1], my_hash.values_at(1)
 2. How would you return the string `"Two"`?
-  return my_hash[:two]
+  my_hash[:two]
 3. How would you return the number `2`?
-  return my_hash["two"]
+  my_hash["two"]
 4. How would you add `{3 => "Three"}` to the hash?
   my_hash[3] = "Three"
 5. How would you add `{:four => 4}` to the hash?
@@ -81,15 +81,15 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 # ```
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-  return users["Jonathan"][:twitter]
+  users["Jonathan"][:twitter]
 2. Return Eriks hometown
-  return users["Erik"][:home_town]
+  users["Erik"][:home_town]
 3. Return the array of Eriks favorite numbers
-  return users["Erik"][:favourite_numbers]
+  users["Erik"][:favourite_numbers]
 4. Return the type of Avrils pet Colin
-  return users["Avril"][:pets]["colin"]
+  users["Avril"][:pets]["colin"]
 5. Return the smallest of Eriks favorite numbers
-  return users["Erik"][:favourite_numbers].min
+  users["Erik"][:favourite_numbers].min
 6. Add the number `7` to Eriks favorite numbers
   users["Erik"][:favourite_numbers].push(7)
 7. Change Eriks hometown to Edinburgh
